41916fc59729b168c4fdf9d0c135915bd757b624
rui6ye <rui6ye@163.com>
2020-10-27 11:20:52 +0800

    e

--- application/user/controller/Article.php
+++ application/user/controller/Article.php
@@ -101,7 +101,7 @@ class Article extends Backend
     }
 
     //写一篇
-    public function add_article() {
+    public function add() {
         $myUid = $this->auth->id;
         if($this->request->isPost()) {
             $rows = input('post.row/a');
