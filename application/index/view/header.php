23892b806d7cdd5d7b16dcb8aaac6517e2d10db8
rui6ye <rui6ye@163.com>
2020-11-10 15:59:29 +0800

    js

--- application/index/view/header.php
+++ application/index/view/header.php
@@ -2,13 +2,14 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<link href="/min/?f=/resource/pub/bootstrap-3.3.7/css/bootstrap.css,/resource/pub/css/jquery.lr_box.css,/resource/pub/css/pub.css" rel="stylesheet" media="all" />
-<link href="/resource/pub/css/jquery.lr_element.css" rel="stylesheet" media="all" />
+<link href="/min/?f=/resource/pub/bootstrap-3.3.7/css/bootstrap.css,/resource/pub/css/pub.css" rel="stylesheet" media="all" />
+<link rel="stylesheet" href="/assets/libs/lr/jquery.lr_box.css" />
+<link rel="stylesheet" href="/assets/libs/lr/jquery.lr_element.css" />
 <script src="/resource/pub/js/jq/jquery-3.2.1.js"></script>
-<script type="text/javascript" src="/resource/pub/js/jquery-lr_base.js"></script>
 <script type="text/javascript" src="/resource/pub/bootstrap-3.3.7/js/bootstrap.js"></script>
-<script type="text/javascript" src="/resource/pub/js/jquery-lr_box.js"></script><!-- LR box -->
-<script type="text/javascript" src="/resource/pub/js/jquery-lr_element.js"></script> <!-- LR element -->
+<script type="text/javascript" src="/assets/libs/lr/jquery-lr_base.js"></script>
+<script type="text/javascript" src="/assets/libs/lr/jquery-lr_box.js"></script><!-- LR box -->
+<script type="text/javascript" src="/assets/libs/lr/jquery-lr_element.js"></script> <!-- LR element -->
 <script type="text/javascript" src="/resource/front/js/front.js"></script><!-- 前端js-->
 <link href="/resource/front/css/front.css" rel="stylesheet" media="all" /> <!-- 模版样式 -->
 <meta property="og:image" content="/resource/front/images/logo.png"/>
