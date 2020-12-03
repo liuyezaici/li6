9283af7796a1796836b2221680f9c4f11a475680
lirui <qbmay@qq.com>
2020-12-04 00:36:09 +0800

    e

--- application/tool/controller/Index.php
+++ application/tool/controller/Index.php
@@ -20,6 +20,9 @@ class Index extends Frontend
     public function _initialize()
     {
         parent::_initialize();
+        header("Access-Control-Allow-Origin:*");
+        header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
+        header("Access-Control-Allow-Headers:DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type, Accept-Language, Origin, Accept-Encoding");
     }
 
     public function saveTask() {
