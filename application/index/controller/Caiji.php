ad09835d3f1f12b1b715b988cb1f64cc81160c0a
rui <rui6ye@163.com>
2021-02-22 16:36:52 +0800

    采集谷歌地图的数据

---
+++ application/index/controller/Caiji.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace app\index\controller;
+
+use app\admin\addon\fujian\model\Fujian;
+use app\common\controller\Frontend;
+use app\common\model\Users;
+use fast\File;
+use fast\Area;
+use fast\Addon;
+use \app\admin\addon\article\model\Article as ArticleModel;
+use \app\admin\addon\article\model\ArticleTypes as ArticleTypesModel;
+use fast\Str;
+
+class Caiji extends Frontend
+{
+
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
+    protected $layout = '';
+    protected $keyword = '';
+    protected $allTypes = [];
+
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->allTypes = ArticleTypesModel::select();
+        $this->keyword = input('keyword', '', 'trim');
+    }
+
+    public function caijiGoogleMap() {
+
+        print_r($this->fetch()) ;
+    }
+}
